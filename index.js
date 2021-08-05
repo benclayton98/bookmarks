4ff9155becbedbf9cd9898619f74f9712ed0fbc7
unknown <benclayton1@hotmail.com>
2021-08-05 15:52:26 +0100

    delete button done

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
+const methodOverride = require('method-override')
 
 const { models } = require('./models/models.js');
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', async (req, res) => {
     const bookmarks = await models.Bookmark.findAll({})
@@ -18,6 +20,15 @@ app.get('/', async (req, res) => {
 app.post('/', async (req, res) => {
     await models.Bookmark.create({
         url: req.body.url
+    })
+    res.redirect('/')
+})
+
+app.delete('/:id', async (req, res) => {
+    await models.Bookmark.destroy({
+        where: {
+            id: req.params.id
+        }
     })
     res.redirect('/')
 })
